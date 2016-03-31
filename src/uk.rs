3482137e551d412cee32d9d9dd4fa67ab2fb25ec
David Hewson <dev@daveid.co.uk>
2016-03-31 21:07:53 +0100

    formatting

--- src/uk.rs
+++ src/uk.rs
@@ -7,8 +7,6 @@ fn easter_ordinal(y: i32) -> u32 {
 }
 
 pub fn is_bankholiday<T: Datelike>(date: &T) -> bool {
-    let day = date.weekday();
-    let (y, m, d) = (date.year(), date.month(), date.day());
 
     let new_years_day = |m, d| m == 1 && d == 1;
     let new_years_sub = |m, d| m == 1 && d <= 3;
@@ -16,26 +14,25 @@ pub fn is_bankholiday<T: Datelike>(date: &T) -> bool {
     let spring = |m, d| m == 5 && 31 - 7 < d;
     let summer = |m, d| m == 8 && 31 - 7 < d;
     let christmas_or_boxingday = |day, m, d| {
-        m == 12 && match day {
+        m == 12 &&
+        match day {
             Weekday::Mon | Weekday::Tue => d >= 25 && d < 29,
             _ => d >= 25 && d < 27,
         }
     };
 
+    let day = date.weekday();
+    let (y, m, d) = (date.year(), date.month(), date.day());
+
     match day {
         Weekday::Sat | Weekday::Sun => false,
         Weekday::Mon => {
-               new_years_sub(m, d)
-            || early_may(m, d)
-            || spring(m, d)
-            || summer(m, d)
-            || christmas_or_boxingday(day, m, d)
-            || easter_ordinal(y) + 1 == date.ordinal()
+            new_years_sub(m, d) || early_may(m, d) || spring(m, d) || summer(m, d) ||
+            christmas_or_boxingday(day, m, d) || easter_ordinal(y) + 1 == date.ordinal()
         }
         _ => {
-               new_years_day(m, d)
-            || christmas_or_boxingday(day, m, d)
-            || easter_ordinal(y) == date.ordinal() + 2
+            new_years_day(m, d) || christmas_or_boxingday(day, m, d) ||
+            easter_ordinal(y) == date.ordinal() + 2
         }
     }
 }
